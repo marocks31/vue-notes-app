bc7bedd15091265910249b6fe8bb8ef104766e0b
marocks31 <mollyarisley@gmail.com>
2022-04-13 22:12:09 -0400

    created NoteSelector component

---
+++ src/views/NoteContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="note-container">
+    <NoteSelectors></NoteSelectors>
+    <NoteEditor></NoteEditor>
+  </div>
+</template>
+
+<script>
+import NoteSelectors from "./NoteSelectors.vue";
+import NoteEditor from "./NoteEditor.vue";
+
+export default {
+  components: {
+    NoteSelectors,
+    NoteEditor,
+  },
+};
+</script>
